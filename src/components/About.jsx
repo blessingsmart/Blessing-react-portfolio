39c66533799bf0d3872a3517b6a322628f44f017
blessingsmart <bublessingsmart@gmail.com>
2023-07-21 15:33:26 +0100

    made the code more mobile responsive (min-h-screen instead of h-screen)

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,20 +2,22 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+    <div name="about" className='w-full min-h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
         <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
                   About  
                 </p>
             </div>
-            <p className='text-xl mt-20'>
-                As a data scientist with a strong expertise in data analysis and visualization, I have honed my skills in transforming raw data into meaningful and actionable visuals that drive crucial business decision-making. With over two years of experience in a business setting, I have successfully analyzed complex datasets, providing valuable insights to enhance organizational efficiency and performance. Beyond my proficiency in data science, I also hold a leadership role in Loveworld Inc (aka Christ Embassy), where I apply data-driven strategies to support the organization's growth and impact.
-            </p>
-            <br/>
-            <p className='text-xl'>
-                As a data scientist with a strong background in frontend development, I possess a passion for education, leading a class of 70+ individuals to excel in Pro Excel and thrive in data-driven environments. Alongside my data science prowess, I'm adept in graphics design using Photoshop and Canva. Certified in business analytics, data science with Python, and CompTIA A+, I'm committed to innovative solutions and making a positive impact through analytics, creative problem-solving, and building dynamic web applications using React and Tailwind.
-            </p>
+            <div>
+              <p className='text-xl mt-20'>
+                  As a data scientist with a strong expertise in data analysis and visualization, I have honed my skills in transforming raw data into meaningful and actionable visuals that drive crucial business decision-making. With over two years of experience in a business setting, I have successfully analyzed complex datasets, providing valuable insights to enhance organizational efficiency and performance. Beyond my proficiency in data science, I also hold a leadership role in Loveworld Inc (aka Christ Embassy), where I apply data-driven strategies to support the organization's growth and impact.
+              </p>
+              <br/>
+              <p className='text-xl'>
+                  As a data scientist with a strong background in frontend development, I possess a passion for education, leading a class of 70+ individuals to excel in Pro Excel and thrive in data-driven environments. Alongside my data science prowess, I'm adept in graphics design using Photoshop and Canva. Certified in business analytics, data science with Python, I'm committed to innovative solutions and making a positive impact through analytics, creative problem-solving, and building dynamic web applications using React and Tailwind.
+              </p>
+            </div>
         </div>
     </div>
   )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
             <div className="flex flex-col justify-center h-full">
-                <h2 className="text-4xl sm:text-7xl font-bold text-white">
+                <h2 className="text-4xl sm:text-7xl font-bold text-white mt-20 md:mt-0">
                     I'm a Data Scientist
                 </h2>
                 <p className="text-gray-500 py-4 max-w-md">i'm skilled in data analysis using Excel, PowerBi and Python. i have 2 years experience analysing and visualizing data. i'm also into frontend web development using Tailwind and React. </p>
